f55e374a9f25b5f069d934d36a5e13a5042d215c
Peerawit Wandech <peerawit.wandech@scb.co.th>
2021-07-08 11:31:43 +0700

    edit code

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+from decrypt import AESCipher
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello World!"
+
+@app.route("/decrypt", methods=['POST'])
+def decrypt():
+    request_data = request.get_json()
+    message = request_data['message']
+    key = request_data['key']
+    dec = AESCipher(message, key).decrypt()
+    return dec
+
+@app.route("/encrypt", methods=['POST'])
+def encrypt():
+    request_data = request.get_json()
+    message = request_data['message']
+    key = request_data['key']
+    enc = AESCipher(message, key).encrypt()
+    return enc
---
+++ decrypt.py
@@ -0,0 +1,32 @@
+from Crypto.Cipher import AES
+from Crypto.Random import new as Random
+from hashlib import sha256
+from base64 import b64encode, b64decode
+
+class AESCipher:
+    def __init__(self, data, gmbKey):
+        self.block_size = 16
+        self.data = data
+        self.key = sha256(gmbKey.encode()).digest()[:16]
+        self.pad = lambda s: s + (self.block_size - len(s) % self.block_size) * \
+            chr(self.block_size - len(s) % self.block_size)
+        self.unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+    def encrypt(self):
+        try:
+            plain_text = self.pad(self.data)
+            iv = Random().read(AES.block_size)
+            cipher = AES.new(self.key, AES.MODE_CBC, iv)
+            return b64encode(iv + cipher.encrypt(plain_text.encode())).decode()
+        except:
+            return 'error, can not encrypt with these data.'
+
+    def decrypt(self):
+        try:
+            cipher_text = b64decode(self.data.encode())
+            iv = cipher_text[:self.block_size]
+            cipher = AES.new(self.key, AES.MODE_CBC, iv)
+            return self.unpad(cipher.decrypt(cipher_text[self.block_size:])).decode()
+        except:
+            return 'error, can not decrypt with these data.'
+
